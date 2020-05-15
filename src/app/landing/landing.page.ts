faa8fe223cfb34827bc8b3e193f118af6e154238
Aurghyadip <aurghya.blog@gmail.com>
2020-05-16 02:27:02 +0530

    added conditional routing to courses page

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -1,3 +1,6 @@
+import { NavController } from '@ionic/angular';
+import { User } from './../models/user/user';
+import { Storage } from '@ionic/storage';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.page.scss'],
 })
 export class LandingPage implements OnInit {
-
-  constructor() { }
+  user: User;
+  constructor(
+    private readonly storage: Storage,
+    private navController: NavController
+  ) { }
 
   ngOnInit() {
+    this.storage.get('user').then((user: string) => {
+      this.user = JSON.parse(user);
+      if (!this.user.user_detail.subscription) {
+        this.navController.navigateForward(['/landing/tabs/courses']);
+      }
+    });
   }
 
 }
