41c90b5fce85c562009ee9c40d8a421f415b77fb
Aurghyadip <aurghya.blog@gmail.com>
2020-05-16 00:40:50 +0530

    added courses page

--- src/app/landing/courses/courses.page.ts
+++ src/app/landing/courses/courses.page.ts
@@ -1,4 +1,9 @@
+import { Course } from './../../models/courses/course';
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { CourseService } from 'src/app/services/course/course.service';
+
+declare var RazorpayCheckout: any;
 
 @Component({
   selector: 'app-courses',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.page.scss'],
 })
 export class CoursesPage implements OnInit {
+  key: string = "rzp_test_FFexwWi4LsHnuc";
+  courses: Course[];
 
-  constructor() { }
+  constructor(
+    private readonly router: NavController,
+    private readonly courseService: CourseService
+  ) { }
 
   ngOnInit() {
+    this.courseService.getAllCourses().subscribe((data: Course[]) => {
+      this.courses = data;
+    });
   }
 
+  showCourseDetails(id: number) {
+    this.router.navigateForward('/courses/main/detail', { queryParams: { id: id } });
+  }
+
+  payWithRazorpay(course: Course) {
+    var options = {
+      description: 'Credits towards consultation',
+      image: 'https://i.imgur.com/3g7nmJC.png',
+      currency: "INR", // your 3 letter currency code
+      key: "rzp_test_1DP5mmOlF5G5ag", // your Key Id from Razorpay dashboard
+      amount: 100, // Payment amount in smallest denomiation e.g. cents for USD
+      name: 'Razorpay',
+      prefill: {
+        email: 'test@razorpay.com',
+        contact: '9990009991',
+        name: 'Razorpay'
+      },
+      theme: {
+        color: '#F37254'
+      },
+      modal: {
+        ondismiss: function () {
+          alert('dismissed')
+        }
+      }
+    };
+    var successCallback = function (payment_id) {
+      alert('payment_id: ' + payment_id);
+    };
+
+    var cancelCallback = function (error) {
+      alert(error.description + ' (Error ' + error.code + ')');
+    };
+
+    RazorpayCheckout.open(options, successCallback, cancelCallback);
+  }
 }
