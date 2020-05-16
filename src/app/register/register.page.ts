ee3eb1caffcfb2dfe20a0b0d6d2bfa6a109e80ed
Aurghyadip <aurghya.blog@gmail.com>
2020-05-16 11:32:05 +0530

    added registration process to app

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,3 +1,8 @@
+import { NavController } from '@ionic/angular';
+import { UserDetail } from './../models/user/user';
+import { UserService } from './../services/user/user.service';
+import { Storage } from '@ionic/storage';
+import { AuthService } from './../services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { v1 as uuidv1 } from 'uuid';
@@ -9,14 +14,18 @@ import { v1 as uuidv1 } from 'uuid';
 })
 export class RegisterPage implements OnInit {
 
-  form: FormGroup;
+  registerForm: FormGroup;
 
   constructor(
-    private readonly formBuilder: FormBuilder
+    private readonly formBuilder: FormBuilder,
+    private readonly authService: AuthService,
+    private readonly storage: Storage,
+    private readonly userService: UserService,
+    private readonly navController: NavController
   ) { }
 
   ngOnInit() {
-    this.form = this.formBuilder.group({
+    this.registerForm = this.formBuilder.group({
       firstname: [null, [Validators.required, Validators.minLength(3)]],
       lastname: [null, [Validators.required, Validators.minLength(3)]],
       mobile_number: [null, [Validators.required, Validators.pattern(/(6|7|8|9)\d{9}/)]],
@@ -29,7 +38,32 @@ export class RegisterPage implements OnInit {
   }
 
   submitRegisterForm() {
-
+    const formData = {
+      firstname: this.registerForm.controls['firstname'].value,
+      lastname: this.registerForm.controls['lastname'].value,
+      mobile_number: this.registerForm.controls['mobile_number'].value,
+      parent_name: this.registerForm.controls['parent_name'].value,
+      school_name: this.registerForm.controls['school_name'].value,
+      address: this.registerForm.controls['address'].value,
+      email: this.registerForm.controls['email'].value,
+      password: this.registerForm.controls['password'].value,
+      username: uuidv1()
+    };
+    this.authService.register(formData.username, formData.email, formData.password).subscribe(
+      (data: any) => {
+        this.storage.set('access_token', data.jwt).then(
+          (x: any) => {
+            this.userService.setUserDetails(formData).subscribe(
+              (data: any) => {
+                this.storage.clear().then((v: any) => {
+                  this.navController.navigateForward(['/home']);
+                });
+              }, (error) => console.log(error)
+            )
+          }
+        );
+      }
+    );
   }
 
 }
